843dbfe042b00e5a3f0177fcc9cd34e7bcc054ca
Nguyen Duong <nguyen.duong@smoothstack.com>
2023-06-16 23:30:17 -0700

    Added table order_detail order_item payment_detail

---
+++ src/db/migration/000003_order.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS "order_detail" CASCADE;
+DROP TABLE IF EXISTS "order_item" CASCADE;
+DROP TABLE IF EXISTS "payment_detail" CASCADE;
+
+
+DROP TYPE IF EXISTS "order_status";
+DROP TYPE IF EXISTS "payment_status";
+DROP TYPE IF EXISTS "payment_type";
